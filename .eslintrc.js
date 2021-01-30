8d1839c36b1bf263b8cf22197b361c356370eee5
pedrolivertwd@gmail.com <pedrolivertwd@gmail.com>
2021-01-29 21:41:38 -0300

    chore: ignore ts inside js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-const { resolve } = require('path');
+const { resolve } = require('path')
 module.exports = {
   // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
   // This option interrupts the configuration hierarchy at this file
@@ -89,6 +89,14 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  overrides: [
+    {
+      "files": ["*.js", "*.jsx"],
+      "rules": {
+        "@typescript-eslint/*": "off"
+      }
+    }
+  ],
 }
