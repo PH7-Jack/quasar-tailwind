437af422b667a15f58025b6d1e3b39380ce642b1
pedrolivertwd@gmail.com <pedrolivertwd@gmail.com>
2021-01-29 21:43:04 -0300

    feat: add tailwindcss

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,8 +1,30 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
+const conflictingClasses = [
+  'order-first',
+  'order-last',
+  'cursor-not-alowed',
+  'cursor-pointer',
+  'block',
+  'inline-block',
+  'text-justify',
+  'hidden',
+  'invisible',
+  'overflow-auto',
+  'overflow-hidden'
+]
+
+const removeClasses = {
+  ...Object.fromEntries(conflictingClasses.map(cssClass => [`.${ cssClass }`, '*'])),
+  body: ['font-family', 'font-size'],
+  '.row, .column, .flex': 'flex-wrap'
+}
+
 module.exports = {
   plugins: [
     // to edit target browsers: use "browserslist" field in package.json
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('postcss-remove-declaration')({ remove: removeClasses }),
+    require('tailwindcss')
   ]
 }
